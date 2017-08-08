273c7d05e72c904e6ce038b638006585d3ace9e3
Felipe Cavalcanti <fjfcavalcanti@gmail.com>
2017-08-08 19:11:07 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,60 @@
+es-count-datadog
+================
+
+### Purpose
+This datadog agent plugin will connect to elasticsearch and count occurrences of configurable terms given a time range (now-interval till now) and push the counts to datadog.
+An example of use case is to send counts of occurrences of events from a logstash and create alarms that will be triggered every time the occurrences have abnormal values.
+
+### Metrics
+This plugin will push a metric called "es-count.count" tagged with the term, stash and time_range, e.g.
+
+```
+ ('es-count.count',
+  1502229650,
+  0,
+  {'hostname': '9b5b7bde1a6a',
+   'tags': ['instance:9b5b7bde1a6a',
+            'term:test2',
+            'stash:test',
+            'time_range:1m'],
+   'type': 'count'})]
+```
+
+es-count.count contains the count of occurrences of {term} in the interval {now-timerange ~ now} on a given index.
+
+**IMPORTANT:** The index must contain time based events and a "timestamp" field for the check to work.
+
+### Tags
+
+Extra tags can be passed to the metrics sent by specifying them into the config file, e.g.
+
+```
+init_config:
+instances:
+  - es_address: http://elasticsearch:9200
+    match:
+      - index: test
+        terms:
+          - test1
+          - test2
+        time_range: 1m
+      - index: test2
+        terms:
+          - test3
+        time_range: 1m
+    tags:
+      - foo
+      - bar
+      - extra:tag
+```
+
+### Development
+
+1. Start elasticsearch
+```
+docker-compose up -d elasticsearch
+```
+2. Execute es-count check
+```
+docker-compose run datadog-agent /opt/datadog-agent/agent/agent.py check es-count
+```
---
+++ checks.d/es-count.py
@@ -0,0 +1,62 @@
+import requests
+import json
+
+from checks import AgentCheck
+
+DEFAULT_ELASTICSEARCH_ADDRESS = 'http://elasticsearch:9200'
+DEFAULT_ELASTICSEARCH_index = 'test'
+DEFAULT_TIME_RANGE = '1m'
+
+class ESCountCheck(AgentCheck):
+    '''
+    Count the number of each term in the last minute
+    '''
+
+    def check(self, instance):
+        self.log.info("starting es-count check")
+        self.es_address = instance.get("es_address", DEFAULT_ELASTICSEARCH_ADDRESS)
+        self.es_index = instance.get("es_index", DEFAULT_ELASTICSEARCH_index)
+        self.log.debug("elasticsearch_address: " + self.es_address)
+        ## get count of each term in the last minute
+        self.extra_tags = instance.get("tags", [])
+        match = instance.get("match", [])
+        tags = ['instance:%s' % self.hostname] + self.extra_tags
+        for target in match:
+            for term in target['terms']:
+                res = self.get_number_of_occurrences('%s/%s/_search' % (self.es_address, target['index']), term, target['time_range'])
+                print res
+                if res != None and u'hits' in res:
+                    occurrences = res[u'hits'][u'total']
+                    self.count('es-count.count', occurrences,
+                            tags=tags + ['term:%s' % term.lower(),
+                                'index:%s' % target['index'].lower(),
+                                'time_range:%s' % target['time_range']])
+
+    def get_number_of_occurrences(self, uri, term, time_range):
+        self.log.debug("getting occurrences for: " + term)
+        query = json.dumps({
+          "query": {
+            "bool": {
+              "must": [{
+                "query_string": {
+                  "query": '%s' % term,
+                  "analyze_wildcard": "true"
+                }
+              }, {
+                "range": {
+                  "@timestamp": {"gt": 'now-%s' % time_range}
+                }
+              }]
+            }
+          },
+          "size": 0
+        })
+        results = None
+        try:
+            response = requests.get(uri, data=query)
+            results = json.loads(response.text)
+        except Exception as e:
+            self.log.error("es_check failed to grab info from es: %s" % str(e))
+        else:
+            self.log.debug("es_check grabbed info from es successfully")
+        return results
